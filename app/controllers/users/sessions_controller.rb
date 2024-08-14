edb304a29450011166b4ae7c82efed7a97f0913c
HP <ferdian@ubuntu.ubuntu-domain>
2024-08-14 10:43:58 +0700

    add fav icon, fix view login and signup, validate pengajuan informasi form

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  layout "auth"
+  layout "login"
   def create
     super do |resource|
       if resource.errors.any?
