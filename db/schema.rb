66cbb435eb3e742ef1a2637f8c0e9f33ee33e5b7
siddiqanshori <siddiq.anshori@gmail.com>
2024-09-09 05:00:39 -0400

    change telepon to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_09_085507) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_09_085925) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -138,7 +138,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_09_085507) do
     t.string "username"
     t.string "ktp"
     t.string "nama_lengkap"
-    t.integer "telepon"
+    t.string "telepon"
     t.string "alamat"
     t.string "pekerjaan"
     t.index ["email"], name: "index_users_on_email", unique: true
