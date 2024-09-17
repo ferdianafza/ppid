e5da22aa4c6b6e83fbe992f4916e33f9145d308e
siddiqanshori <siddiq.anshori@gmail.com>
2024-09-17 23:49:19 +0700

    update keterangan file  permohonan informasidan regex Nama Lengkap

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
   # validates :ktp, presence: true, length: { minimum: 16, maximum: 16}
   validates :ktp, presence: true, length: { is: 16 }
 
-  
+  # Validasi untuk memastikan nama lengkap hanya mengandung huruf
+  validates :nama_lengkap, format: { with: /\A[a-zA-Z\s]+\z/, message: "hanya boleh mengandung huruf dan spasi" }
+
   validates :telepon, presence: true, length: { minimum: 10, maximum: 13}
 
   def self.ransackable_attributes(auth_object = nil)
