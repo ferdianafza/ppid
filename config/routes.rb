d49c1ed776ca2decca98538e0ec19c96363bdfcc
HP <ferdian@ubuntu.ubuntu-domain>
2024-11-07 13:32:19 +0700

    fix route artikels

--- config/routes.rb
+++ config/routes.rb
@@ -23,12 +23,11 @@ Rails.application.routes.draw do
       get :increment_access
     end
   end
-  resources :artikels
+  resources :artikels, except: [:index]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :pengajuaninformasis, except: [:show, :index]
   devise_for :users, :controllers => { registrations: 'users/registrations', sessions: 'users/sessions' }
-  # devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations'}
   devise_scope :user do
     get 'sign_in', to: 'devise/sessions/#new'
     get '/users/sign_out' => 'devise/sessions#destroy'
